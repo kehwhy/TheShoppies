df778b219f38e4120818dd6e55b36622bf826399
Katie Younge <youngekatie@gmail.com>
2020-12-24 01:01:13 -0500

    undoing save to local storage

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import './App.css';
 import ConfettiContainer from './features/ConfettiContainer/ConfettiContainer'
 import NomineeFinder from './features/NomineeFinder/NomineeFinder';
 import NomineeList from './features/NomineeList/NomineeList';
-import { closeDialog, openDialog, selectIsDialogShown, setNominees } from './features/NomineeList/nomineeListSlice';
+import { closeDialog, openDialog, selectIsDialogShown } from './features/NomineeList/nomineeListSlice';
 import title from './title.png'
 
 function App() {
   const dispatch = useDispatch()
   const isShown = useSelector(selectIsDialogShown)
-
-  console.log(localStorage.getItem("MyNomineeList"))
-
-  if (localStorage.getItem("MyNomineeList")){
-    dispatch(setNominees(JSON.parse(localStorage.getItem("MyNomineeList"))))
-  }
-
   return (
     <div className="App">
       <ConfettiContainer />
